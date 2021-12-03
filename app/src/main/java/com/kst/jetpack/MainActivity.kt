8d06e3f7af1faf15b67daaa1252bbbf0a9fa04bf
jason <readonly23@gmail.com>
2021-12-03 21:01:48 +0000

    update setting

--- app/src/main/java/com/kst/jetpack/MainActivity.kt
+++ app/src/main/java/com/kst/jetpack/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetPackTheme {
+            JetPackTheme(false) {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
